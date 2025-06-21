915fd9677900ffb66568b26772434eaba7438507
cousinbigcountry <fountainkylec@gmail.com>
2025-06-21 01:59:58 -0400

    Inital commit

---
+++ src/main/java/com/tylerspub/revamped/TylerspubRevampedApplication.java
@@ -0,0 +1,13 @@
+package com.tylerspub.revamped;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TylerspubRevampedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TylerspubRevampedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tylerspub/revamped/TylerspubRevampedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tylerspub.revamped;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TylerspubRevampedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
